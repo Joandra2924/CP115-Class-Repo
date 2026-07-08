57fe82fad92ca2de13f306ba22bba2a29e364ef3
Joandra2924 <putrij0916@gmail.com>
2026-07-08 12:26:13 +0800

    lab 03 exercise 4

---
+++ labs/lab03/exercise3/lab-03-3.py
@@ -0,0 +1,9 @@
+kilobyte = float(input())
+megabyte = kilobyte / 1024
+print(megabyte)
+gigabyte = kilobyte / 1024 ** 2
+print(gigabyte)
+terabyte = kilobyte / 1024 ** 3
+print(terabyte)
+petabyte = kilobyte / 1024 ** 4
+print(petabyte)
